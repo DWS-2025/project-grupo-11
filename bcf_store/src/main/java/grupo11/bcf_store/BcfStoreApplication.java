ec2267855b0fc577bb45d085a2c0d49981707189
Carlosgcov <carlosgcov@gmail.com>
2025-02-14 12:18:12 +0100

    Subida Inicial

---
+++ bcf_store/src/main/java/grupo11/bcf_store/BcfStoreApplication.java
@@ -0,0 +1,13 @@
+package grupo11.bcf_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcfStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcfStoreApplication.class, args);
+	}
+
+}
---
+++ bcf_store/src/test/java/grupo11/bcf_store/BcfStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo11.bcf_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcfStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
