4c01a6506da5a773638f9fe41be957c447b7a24e
marioserraano <marioserranoygarcia@gmail.com>
2025-03-23 17:54:57 +0100

    Arreglo consola h2 y eliminación carrito por defecto

--- src/main/java/grupo11/bcf_store/service/SampleDataService.java
+++ src/main/java/grupo11/bcf_store/service/SampleDataService.java
@@ -3,10 +3,8 @@ package grupo11.bcf_store.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import grupo11.bcf_store.model.Cart;
 import grupo11.bcf_store.model.Product;
 import grupo11.bcf_store.model.User;
-import grupo11.bcf_store.repository.CartRepository;
 import grupo11.bcf_store.repository.ProductRepository;
 import grupo11.bcf_store.repository.UserRepository;
 
@@ -21,9 +19,6 @@ public class SampleDataService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private CartRepository cartRepository;
-
     @PostConstruct
     public void init() {
         // Default user
@@ -37,9 +32,5 @@ public class SampleDataService {
         productRepository.save(new Product("BUFANDA BURGOS CF", 20, "Bufanda con el nombre y escudo del equipo", "/images/scarf.jpg"));
         productRepository.save(new Product("BALON BLANQUINEGRO", 20, "Disfruta del balón de tu equipo", "/images/balon.jpg"));
         productRepository.save(new Product("BABERO BLANQUINEGRO", 20, "Babero con escudo del equipo", "/images/babero.jpg"));
-
-        //Default cart
-        Cart cart = new Cart();
-        cartRepository.save(cart);
     }
 }
