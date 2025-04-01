56827cdb21d13ea9aef59b9e9203ab57c0ae3dbf
marioserraano <marioserranoygarcia@gmail.com>
2025-04-01 09:32:24 +0200

    Arreglo Funcionamiento Mappers

--- src/main/java/grupo11/bcf_store/model/ProductDTO.java
+++ src/main/java/grupo11/bcf_store/model/ProductDTO.java
@@ -1,11 +1,13 @@
 package grupo11.bcf_store.model;
 
 import java.sql.Blob;
+import java.util.List;
 
 public record ProductDTO(
         Long id,
         String name,
         double price,
         String description,
-        Blob imageFile) {
+        Blob imageFile,
+        List<Order> orders) {
 }
