245cb65de062aac0a712006b43984601d3cb9bac
Carlosgcov <carlosgcov@gmail.com>
2025-04-02 20:19:31 +0200

    Añadidos más productos por defecto

--- src/main/java/grupo11/bcf_store/service/SampleDataService.java
+++ src/main/java/grupo11/bcf_store/service/SampleDataService.java
@@ -31,33 +31,27 @@ public class SampleDataService {
         userRepository.save(new User("m.serranog.2023@alumnos.urjc.es", "hola"));
 
         // Default products
-        productRepository.save(new Product("1ª EQUIPACION BURGOS CF 24/25", 60, "Camiseta oficial de partido.", productService.getImageBlob("/static/images/shirt0.jpg")));
-        productRepository.save(new Product("3º EQUIPACION BURGOS CF 24/25", 60, "Camiseta oficial de partido.", productService.getImageBlob("/static/images/shirt1.jpg")));
-        productRepository.save(new Product("CHUBASQUERO NEGRO 23/24", 60, "Chubasquero del Burgos CF.", productService.getImageBlob("/static/images/chubasquero.jpg")));
-        productRepository.save(new Product("PARKA BURGOS CF 23/24", 60, "Parka con el escudo del equipo.", productService.getImageBlob("/static/images/parka.jpg")));
+        productRepository.save(new Product("1ª EQUIPACION BURGOS CF 24/25", 60, "Camiseta oficial de partido", productService.getImageBlob("/static/images/shirt0.jpg")));
+        productRepository.save(new Product("2ª EQUIPACION BURGOS CF 24/25", 60, "Camiseta oficial de partido", productService.getImageBlob("/static/images/shirt1.jpg")));
+        productRepository.save(new Product("3º EQUIPACION BURGOS CF 24/25", 60, "Camiseta oficial de partido", productService.getImageBlob("/static/images/shirt2.jpg")));
+        productRepository.save(new Product("CAMISETA BCF LOVE", 20, "Regalo perfecto para San Valentín", productService.getImageBlob("/static/images/shirtL.jpg")));
+        productRepository.save(new Product("CAMISETA BCF VIDA", 20, "Demuestra tu afición por el BCF", productService.getImageBlob("/static/images/shirtV.jpg")));
+        productRepository.save(new Product("CHUBASQUERO NEGRO 23/24", 60, "Chubasquero del Burgos CF", productService.getImageBlob("/static/images/chubasquero.jpg")));
+        productRepository.save(new Product("PARKA BURGOS CF 23/24", 60, "Parka con el escudo del equipo", productService.getImageBlob("/static/images/parka.jpg")));
+        productRepository.save(new Product("PANTALON BURGOS CF 22/23", 30, "Pantalón con el escudo del equipo", productService.getImageBlob("/static/images/joggers.jpg")));
         productRepository.save(new Product("BUFANDA BURGOS CF", 20, "Bufanda con el nombre y escudo del equipo", productService.getImageBlob("/static/images/scarf.jpg")));
         productRepository.save(new Product("BALON BLANQUINEGRO", 20, "Disfruta del balón de tu equipo", productService.getImageBlob("/static/images/balon.jpg")));
-        productRepository.save(new Product("BABERO BLANQUINEGRO", 20, "Babero con escudo del equipo", productService.getImageBlob("/static/images/babero.jpg")));
-        productRepository.save(new Product("1ª EQUIPACION BURGOS CF 24/25", 60, "Camiseta oficial de partido.", productService.getImageBlob("/static/images/shirt0.jpg")));
-        productRepository.save(new Product("3º EQUIPACION BURGOS CF 24/25", 60, "Camiseta oficial de partido.", productService.getImageBlob("/static/images/shirt1.jpg")));
-        productRepository.save(new Product("CHUBASQUERO NEGRO 23/24", 60, "Chubasquero del Burgos CF.", productService.getImageBlob("/static/images/chubasquero.jpg")));
-        productRepository.save(new Product("PARKA BURGOS CF 23/24", 60, "Parka con el escudo del equipo.", productService.getImageBlob("/static/images/parka.jpg")));
-        productRepository.save(new Product("BUFANDA BURGOS CF", 20, "Bufanda con el nombre y escudo del equipo", productService.getImageBlob("/static/images/scarf.jpg")));
-        productRepository.save(new Product("BALON BLANQUINEGRO", 20, "Disfruta del balón de tu equipo", productService.getImageBlob("/static/images/balon.jpg")));
-        productRepository.save(new Product("BABERO BLANQUINEGRO", 20, "Babero con escudo del equipo", productService.getImageBlob("/static/images/babero.jpg")));
-        productRepository.save(new Product("1ª EQUIPACION BURGOS CF 24/25", 60, "Camiseta oficial de partido.", productService.getImageBlob("/static/images/shirt0.jpg")));
-        productRepository.save(new Product("3º EQUIPACION BURGOS CF 24/25", 60, "Camiseta oficial de partido.", productService.getImageBlob("/static/images/shirt1.jpg")));
-        productRepository.save(new Product("CHUBASQUERO NEGRO 23/24", 60, "Chubasquero del Burgos CF.", productService.getImageBlob("/static/images/chubasquero.jpg")));
-        productRepository.save(new Product("PARKA BURGOS CF 23/24", 60, "Parka con el escudo del equipo.", productService.getImageBlob("/static/images/parka.jpg")));
-        productRepository.save(new Product("BUFANDA BURGOS CF", 20, "Bufanda con el nombre y escudo del equipo", productService.getImageBlob("/static/images/scarf.jpg")));
-        productRepository.save(new Product("BALON BLANQUINEGRO", 20, "Disfruta del balón de tu equipo", productService.getImageBlob("/static/images/balon.jpg")));
-        productRepository.save(new Product("BABERO BLANQUINEGRO", 20, "Babero con escudo del equipo", productService.getImageBlob("/static/images/babero.jpg")));
-        productRepository.save(new Product("1ª EQUIPACION BURGOS CF 24/25", 60, "Camiseta oficial de partido.", productService.getImageBlob("/static/images/shirt0.jpg")));
-        productRepository.save(new Product("3º EQUIPACION BURGOS CF 24/25", 60, "Camiseta oficial de partido.", productService.getImageBlob("/static/images/shirt1.jpg")));
-        productRepository.save(new Product("CHUBASQUERO NEGRO 23/24", 60, "Chubasquero del Burgos CF.", productService.getImageBlob("/static/images/chubasquero.jpg")));
-        productRepository.save(new Product("PARKA BURGOS CF 23/24", 60, "Parka con el escudo del equipo.", productService.getImageBlob("/static/images/parka.jpg")));
-        productRepository.save(new Product("BUFANDA BURGOS CF", 20, "Bufanda con el nombre y escudo del equipo", productService.getImageBlob("/static/images/scarf.jpg")));
-        productRepository.save(new Product("BALON BLANQUINEGRO", 20, "Disfruta del balón de tu equipo", productService.getImageBlob("/static/images/balon.jpg")));
-        productRepository.save(new Product("BABERO BLANQUINEGRO", 20, "Babero con escudo del equipo", productService.getImageBlob("/static/images/babero.jpg")));
+        productRepository.save(new Product("CALCETINES BCF", 10, "Calcetines oficiales del BCF", productService.getImageBlob("/static/images/socks.jpg")));
+        productRepository.save(new Product("ABRIDOR BCF", 5, "Abridor con imán del club", productService.getImageBlob("/static/images/opener.jpg")));
+        productRepository.save(new Product("BANDERIN BCF", 10, "Banderin para colocar en coche", productService.getImageBlob("/static/images/pennant.jpg")));
+        productRepository.save(new Product("BOLSA BCF", 10, "Bolsa del club", productService.getImageBlob("/static/images/bag.jpg")));
+        productRepository.save(new Product("BOLIGRAFO BCF", 2, "Boligrafo del club", productService.getImageBlob("/static/images/pen.jpg")));
+        productRepository.save(new Product("BRAGA CUELLO BCF", 12, "Braga de cuello del BCF", productService.getImageBlob("/static/images/ngaiter.jpg")));
+        productRepository.save(new Product("GAFAS BCF", 30, "Protege tus ojos con estilo", productService.getImageBlob("/static/images/glasses.jpg")));
+        productRepository.save(new Product("GUANTES LANA BCF", 30, "Abriga tus manos con los guantes blanquinegros", productService.getImageBlob("/static/images/gloves.jpg")));
+        productRepository.save(new Product("LLAVERO DORADO BCF", 5, "Llavero del BCF", productService.getImageBlob("/static/images/keychain.jpg")));
+        productRepository.save(new Product("MOCHILA ADIDAS BCF", 50, "Mochila Adidas del club", productService.getImageBlob("/static/images/backp.jpg")));
+        productRepository.save(new Product("VISERA BCF", 10, "Gorra con el escudo del Burgos", productService.getImageBlob("/static/images/cap.jpg")));
+        productRepository.save(new Product("PELUCHE OSO BCF", 20, "Abraza la calidez y ternura", productService.getImageBlob("/static/images/bear.jpg")));
     }
 }
