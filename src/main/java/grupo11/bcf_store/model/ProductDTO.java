a45c7c02e0a3b7ff3bee8aa65bafce38fb295737
marioserraano <marioserranoygarcia@gmail.com>
2025-04-04 11:24:40 +0200

    Eliminación de Blob en ProductDTO

--- src/main/java/grupo11/bcf_store/model/ProductDTO.java
+++ src/main/java/grupo11/bcf_store/model/ProductDTO.java
@@ -1,6 +1,5 @@
 package grupo11.bcf_store.model;
 
-import java.sql.Blob;
 import java.util.List;
 
 public record ProductDTO(
@@ -8,7 +7,6 @@ public record ProductDTO(
         String name,
         double price,
         String description,
-        Blob imageFile,
         String image,
         List<Order> orders) {
 }
